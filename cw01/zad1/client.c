46d8b91f5fdf0d0d9f8af84b91351ff86efc08af
Maciej Grzybacz <128727060+MaciejGrzybacz@users.noreply.github.com>
2024-03-11 20:33:05 +0100

    lab1

--- cw01/zad1/client.c
+++ cw01/zad1/client.c
@@ -1,20 +1,25 @@
 //
-// Created by maciejgrzybacz on 06.03.24.
+// Created by maciejgrzybacz on 11.03.24.
 //
-
-#include <stdio.h>
+#ifndef USE_DYNAMIC_LIBRARY
+#include "collatz_library/collatz.h"
+#else
 #include <dlfcn.h>
 
+int (*collatz_conjecture)(int input);
+int (*test_collatz_convergence)(int input, int max_iter);
+#endif
+
+#include <stdio.h>
+
 int main() {
+#ifdef USE_DYNAMIC_LIBRARY
     char path[] = "collatz_library/collatz.so";
     void* handle = dlopen(path,RTLD_LAZY);
     if(!handle) {
         printf("Cannnot load a libary placed in %s", path);
         return 1;
     }
-
-    int (*collatz_conjecture)(int input);
-    int (*test_collatz_convergence)(int input, int max_iter);
 
     collatz_conjecture = dlsym(handle, "collatz_conjecture");
     test_collatz_convergence = dlsym(handle, "test_collatz_convergence");
@@ -28,17 +33,18 @@ int main() {
         return 1;
     }
 
+#endif
     int max_iter = 10;
-#ifdef USE_DYNAMIC_LIBRARY
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 5; i++) {
         int collatz_convergence = test_collatz_convergence(i, max_iter);
         if (collatz_convergence >= 0)
             printf("Collatz conjecture converges for %d after %d operations.\n", i, collatz_convergence);
         else
             printf("Collatz conjecture for %d is unreachable after %d operations.\n", i, max_iter);
     }
-#else
-    printf("USE_DYNAMIC_LIBRARY is not defined. Please define it to use dynamic library.\n");
+
+#ifdef USE_DYNAMIC_LIBRARY
+    dlclose(handle);
 #endif
 
     return 0;
